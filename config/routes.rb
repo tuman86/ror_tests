72389ad04e63da796bc5f295ac8229c8d43d9d10
som <chandelmeenaxi@gmail.com>
2018-05-29 11:12:20 +0530

    working on layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get  'static_pages/about'
+  get 'users/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/home', to: 'static_pages#home'
+  get  '/signup',  to: 'users#new'
 end
